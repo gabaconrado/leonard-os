1e64197804027f547b0375074f6aa9e6d6cbafe9
Gabriel Conrado <gabaconrado@gmail.com>
2020-07-12 16:52:13 -0300

    VGA Buffer - part 1

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Hello World!";
+
+mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
@@ -12,14 +13,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
