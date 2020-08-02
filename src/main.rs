9614d6fb3cc78f5d551ddae98860afbd2970d035
pedrosmv <pedrosmv@gmail.com>
2020-08-02 16:10:09 -0300

    Finishing VGA Text Buffer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+extern crate rlibc;
 
 extern crate rlibc;
 use core::panic::PanicInfo;
@@ -9,12 +10,14 @@ mod vga_buffer;
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+
     loop {}
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
 
     loop {}
 }
