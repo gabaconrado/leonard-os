74e9d06cb0b1ba7c93df9c4afda8e4efb2fed641
pedrosmv <pedrosmv@gmail.com>
2020-09-13 16:12:18 -0300

    Finishing Testing module

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,4 +1,3 @@
-// src/main.rs
 
 #![no_std]
 #![no_main]
@@ -7,28 +6,26 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use leonard_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     leonard_os::test_panic_handler(info)
+}
+
+use leonard_os::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
